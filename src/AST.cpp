d6bbd70cc0eca4e692663af1e7c299c72cac842d
Bastien Marsaud <bastien@mouseover.fr>
2020-06-19 12:45:35 +0200

    doc: add AST conversion comments

--- src/AST.cpp
+++ src/AST.cpp
@@ -16,10 +16,15 @@
 // 3   4 5   6
 //
 
+// The graph browsing algorithm is post-order
+// First, we browse all the left nodes
+// Next, we browse all the right nodes
+// Finnaly, we handle the current node
 string ASTExpr::toASM() {
     ASSM assm;
     string result;
 
+    // All left nodes browsing
     if(this->left != nullptr) {
         if(this->left->type != EXPR) {
             result.append(assm.registerToRegister(this->left->toASM(), ASSM::REGISTER_A)).append("\n");
@@ -28,6 +33,7 @@ string ASTExpr::toASM() {
         }
     }
 
+    // All right nodes browsing
     if(this->right != nullptr) {
         if(this->right->type != EXPR) {
             result.append(assm.registerToRegister(this->right->toASM(), ASSM::REGISTER_B)).append("\n");
@@ -36,6 +42,7 @@ string ASTExpr::toASM() {
         }
     }
 
+    // Handle current node
     if(this->type == EXPR) {
         bool isRootNode = this->parent == nullptr;
         bool isLeftExpr = false;
@@ -44,9 +51,13 @@ string ASTExpr::toASM() {
             isLeftExpr = parentExpr->left == this;
         }
 
+        // If left branch put the result in register A, if right branch put the result in register B
+        // If we are the root node put the result in register A for legacy integration
         string outputReg = isRootNode || isLeftExpr ? ASSM::REGISTER_A : ASSM::REGISTER_B;
 
+        // If my left branch is an expression, an operation result has been stashed to register D
         if(this->left->type == EXPR) {
+            // Pop operation result from register D to register A
             result.append(ASSM::INDENT).append(assm.registerToRegister(ASSM::REGISTER_D, ASSM::REGISTER_A)).append("\n");
         }
 
@@ -64,7 +75,9 @@ string ASTExpr::toASM() {
             result.append(ASSM::INDENT).append("idivl ").append(ASSM::REGISTER_B).append(", ").append(outputReg).append("\n");
         }
 
+        // If I am a left expression I have to stash the operation result to register D to avoid its deletion by the right branch
         if(isLeftExpr && !isRootNode) {
+            // Stash operation result from register A to register D
             result.append(ASSM::INDENT).append(assm.registerToRegister(ASSM::REGISTER_A, ASSM::REGISTER_D)).append("\n");
         }
     }
